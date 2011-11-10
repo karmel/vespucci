8d7098134e102640fa02db27c459764605c081ae
Karmel Allison <karmel@arcaio.com>
2011-11-09 16:24:07 -0800

    - Modifying strain SNP script

--- trunk/glasslab/utils/misc/filter_vcf.py
+++ trunk/glasslab/utils/misc/filter_vcf.py
@@ -14,20 +14,25 @@ def filter_variants(file_name, output_file=None):
     output = output_file or (file_name + '.clean')
     o1 = file(output, 'w')
     
-    strains = (14,) #(14, 16, 19, 21)
-            
+    strains = (14, 19, 21)
+    inc_reference = (14, 16, 19, 21)
     for line in f:
         fields = line.split('\t')
         if line[:4] == '#CHR':
             # Header
-            write_line(fields[:5] + [fields[i] for i in strains], o1)
+            write_line(fields[:5] + [fields[i] for i in inc_reference], o1)
             continue
         elif line[0] == '#': continue
+        elif int(fields[16].split(':')[1]) != 0: 
+            # Format:  GT:ATG:MQ:HCG:GQ:DP
+            # We want ATG == 0 for black6, since that means "of quality, and matches reference"
+            continue
         else:
-            for strain_of_interest in strains: # BALB, Bl6, NOD, DBA
+            for strain_of_interest in strains: # BALB, NOD, DBA
                 try:
-                    if int(fields[strain_of_interest][0]) > 0: 
-                        write_line(fields[:5] + [fields[i] for i in strains], o1)
+                    if int(fields[strain_of_interest].split(':')[1]) > 0: 
+                        # We want ATG == 1 for the strains, since that means "of quality, and doesn't match reference"
+                        write_line(fields[:5] + [fields[i] for i in inc_reference], o1)
                         break
                 except ValueError: pass
 
@@ -42,7 +47,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1: file_name = sys.argv[1]
     else: file_name = '/Users/karmel/Downloads/20110602-final-snps.vcf'
     if len(sys.argv) > 2: output_file = sys.argv[2]
-    else: output_file = '/Users/karmel/Downloads/20110602-final-snps_balb.vcf'
+    else: output_file = '/Users/karmel/Downloads/20110602-final-snps_strain_only_cleaned.vcf'
     
     filter_variants(file_name, output_file)
 
