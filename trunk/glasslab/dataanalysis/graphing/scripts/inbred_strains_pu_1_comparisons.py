adfdbe0f61438ec56d2ded884063ffa6c40641ed
Karmel Allison <karmel@arcaio.com>
2012-03-28 21:27:26 -0700

    - Filtering out low scoring peaks for PU.1 comparisons

--- trunk/glasslab/dataanalysis/graphing/scripts/inbred_strains_pu_1_comparisons.py
+++ trunk/glasslab/dataanalysis/graphing/scripts/inbred_strains_pu_1_comparisons.py
@@ -19,9 +19,13 @@ if __name__ == '__main__':
     
     wt_peaks, balb_peaks, nod_peaks, balb2_peaks = 67074, 79353, 107716, 94199
     
-    data = grapher.normalize(data, 'balb_pu_1_tag_count', balb_peaks/wt_peaks)
-    data = grapher.normalize(data, 'nod_pu_1_tag_count', nod_peaks/wt_peaks)
-    data = grapher.normalize(data, 'balb2_pu_1_tag_count', balb2_peaks/wt_peaks)
+    data = grapher.normalize(data, 'balb_pu_1_tag_count', 1)#balb_peaks/wt_peaks)
+    data = grapher.normalize(data, 'nod_pu_1_tag_count', 1)#nod_peaks/wt_peaks)
+    data = grapher.normalize(data, 'balb2_pu_1_tag_count', 1)#balb2_peaks/wt_peaks)
+    
+    for i, row in data[data['wt_pu_1_tag_count'] < 20].iterrows(): data['wt_pu_1_tag_count'][i] = 0
+    for i, row in data[data['balb_pu_1_tag_count_norm'] < 20].iterrows(): data['balb_pu_1_tag_count_norm'][i] = 0
+    for i, row in data[data['nod_pu_1_tag_count_norm'] < 20].iterrows(): data['nod_pu_1_tag_count_norm'][i] = 0
     
     data['wt_to_balb'] = data['wt_pu_1_tag_count']/data['balb_pu_1_tag_count']
     data['nod_to_balb'] = data['nod_pu_1_tag_count']/data['balb2_pu_1_tag_count']
@@ -50,7 +54,7 @@ if __name__ == '__main__':
                             show_2x_range=False, show_legend=True, text_color=True, 
                             show_count=True, show_correlation=True, show_plot=False, ax=ax)
         #ax.set_ylim(4,128)
-        grapher.save_plot(os.path.join(dirpath, 'bl6_vs_nod_pu_1_peak_tag_counts_bl6_gt_balb_unique.png'))
+        grapher.save_plot(os.path.join(dirpath, 'bl6_vs_nod_pu_1_peak_tag_counts_bl6_gt_balb.png'))
         grapher.show_plot()
     
     if True:
@@ -64,7 +68,7 @@ if __name__ == '__main__':
                                   title='PU.1 peak tags where BALBc has a SNP that ruins its PU.1 Motif', 
                                   xlabel='', ylabel='Tags per PU.1 peak', 
                                   show_outliers=False, show_plot=False)
-        grapher.save_plot(os.path.join(dirpath, 'peak_boxplots_no_balb_motif_norm_by_tot_peaks.png'))
+        grapher.save_plot(os.path.join(dirpath, 'peak_boxplots_no_balb_motif_filter_low_peaks.png'))
         grapher.show_plot()
         
     print 'p-val that BALBc is different than C57Bl6: %g' % ttest_ind(data['wt_pu_1_tag_count'],data['balb_pu_1_tag_count_norm'])[1]
