daa6265ce2e65329a1849da45dc1df5c27e14bfd
Karmel Allison <karmel@arcaio.com>
2012-06-02 21:59:53 -0700

    - Motif analysis

--- trunk/glasslab/dataanalysis/motifs/motif_analyzer.py
+++ trunk/glasslab/dataanalysis/motifs/motif_analyzer.py
@@ -115,9 +115,9 @@ if __name__ == '__main__':
     #data = data[data['h3k4me2_notx_score'] > 0]
     #data = data[data['length'] > 200]
     #data = data[abs(data['balb_plating_notx_fc']) < 1]
-    #data = data[data['balb_nod_notx_1h_fc'] >= 1]
+    data = data[data['balb_nod_notx_1h_fc'] >= 1]
     
     #data = yzer.collapse_strands(data)
     
-    yzer.run_homer(data, 'refseq_all_stranded', dirpath, 
-                   cpus=6, center=False, reverse=False, size=200, length=[8,10,12])
+    yzer.run_homer(data, 'nondiabetic_nod_notx_up_refseq', dirpath, 
+                   cpus=6, center=False, reverse=False, size=200, length=[8,10,12], bg=bg)
