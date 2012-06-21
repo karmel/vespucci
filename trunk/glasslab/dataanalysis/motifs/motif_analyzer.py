262634ba1c218954c5295336c60ee599fa56c432
Karmel Allison <karmel@arcaio.com>
2012-06-21 18:48:15 +0300

    - Motif analyzing for NOD/BALBc

--- trunk/glasslab/dataanalysis/motifs/motif_analyzer.py
+++ trunk/glasslab/dataanalysis/motifs/motif_analyzer.py
@@ -124,7 +124,7 @@ if __name__ == '__main__':
     data = data[data['h3k4me2_notx_score'] > 0]
     data = data[data['length'] > 200]
     #data = data[abs(data['balb_plating_notx_fc']) < 1]
-    data = data[data['balb_nod_notx_1h_fc'] >= -1]
+    data = data[data['balb_nod_notx_1h_fc'] >= 1]
     
     data = yzer.collapse_strands(data)
     
