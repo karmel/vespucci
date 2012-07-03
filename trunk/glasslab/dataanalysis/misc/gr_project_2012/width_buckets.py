b973b0a10c8c08be99d2190d8fa048b4b80be328
Karmel Allison <karmel@arcaio.com>
2012-07-03 09:38:46 +0300

    - Graphing for GR project: width buckets

--- trunk/glasslab/dataanalysis/misc/gr_project_2012/width_buckets.py
+++ trunk/glasslab/dataanalysis/misc/gr_project_2012/width_buckets.py
@@ -9,6 +9,31 @@ from glasslab.dataanalysis.misc.gr_project_2012.elongation import draw_elongatio
 from glasslab.dataanalysis.graphing.basepair_counter import BasepairCounter
 import os
 
+def get_tag_proportions(all_data, subset, subset_label):
+    
+    for label, data in (('All',all_data), (subset_label,subset)):
+        total_tags = sum(data['tag_count'])
+        print 'Total tags in {0}: {1}'.format(label, total_tags)
+        
+        states = (('Special', 'group_{0}'),
+                  ('KLA','kla_{0}state'), ('KLA+Dex','kla_dex_{0}state'),
+                  ('KLA+Dex over KLA','dex_over_kla_{0}state'),)
+        for desc,state in states:
+            for replicate_id in ('',1,2,3,4):
+                rep_str = get_rep_string(replicate_id)
+                state_str = state.format(rep_str)
+                datasets = [('No change in {0}'.format(desc), data[data[state_str] == 0]),
+                            ('Up in {0}'.format(desc), data[data[state_str] == 1]),
+                            ('Down in {0}'.format(desc), data[data[state_str] == -1]),
+                            ('Up > 2x in KLA, Down > 1.5x from that in Dex', 
+                             data[(data['kla_{0}state'.format(rep_str)] == 1)
+                                  & (data['dex_over_kla_{0}state'.format(rep_str)] == -1)]),]
+                for name, dataset in datasets:
+                    dataset_tags = sum(dataset['total_tags'])
+                    print 'Total tags in {0}: {1}'.format(name, dataset_tags)
+                    print 'Percent of total tags in {0}: {1}'.format(name, dataset_tags/total_tags)
+            
+        
 if __name__ == '__main__':
     
     grapher = BasepairCounter()
@@ -48,6 +73,7 @@ if __name__ == '__main__':
         datasets = [#('not_paused_dmso_15', data[data['dmso_bucket_score'] <= .15]),
                     #('not_paused_kla_15', data[data['kla_bucket_score'] <= .15]),
                     #('not_paused_kla_dex_15', data[data['kla_dex_bucket_score'] <= .15]),
+                    ('all_refseq', data),
                     ('kla_dex_more_paused_than_kla_01', 
                         data[data['kla_dex_bucket_score'] - data['kla_bucket_score'] >= .01 ]),
                     
@@ -64,13 +90,15 @@ if __name__ == '__main__':
         
 
         for name, dataset in datasets:
-            print len(dataset)
+            get_tag_proportions(data, dataset, name)
+            
+            '''
             curr_dirpath = grapher.get_filename(dirpath, name)
             if not os.path.exists(curr_dirpath): os.mkdir(curr_dirpath)
             
             draw_elongation_profile(dataset, grapher, curr_dirpath, 
                                     show_moving_average=False, show_count=True)
     
-            
+            '''
             
         
