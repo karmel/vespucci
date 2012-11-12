a72a5caa34bbdc16c7d9442d50ab32390d887b7b
Karmel Allison <karmel@arcaio.com>
2012-11-12 13:49:47 -0800

    - Data import for genome resources

---
+++ trunk/glasslab/genomereference/pipeline/set_up_database.py
@@ -0,0 +1,36 @@
+'''
+Created on Feb 23, 2011
+
+@author: karmel
+'''
+from glasslab.utils.database import execute_query
+from optparse import make_option
+from glasslab.glassatlas.datatypes.transcript import CellTypeBase
+from glasslab.glassatlas.pipeline.base_parser import GlassAtlasParser
+from glasslab.genomereference.sql.sql_generator import GenomeResourcesSqlGenerator
+
+class SetUpDatabaseParser(GlassAtlasParser):
+    options = [
+               make_option('-g', '--genome',action='store', type='string', dest='genome', default='mm9', 
+                           help='Currently supported: mm8, mm8r, mm9, hg18, hg18r'),
+               make_option('-c', '--cell_type',action='store', type='string', dest='cell_type', 
+                           help='Cell type for this run? Options are: %s' % ','.join(CellTypeBase.get_correlations().keys())),
+               make_option('--schema_only', action='store_true', dest='schema_only', default=False,
+                           help='Only create schema and stop before importing all the data?'),
+                ]
+
+if __name__ == '__main__':
+    parser = SetUpDatabaseParser()
+    options, args = parser.parse_args()
+    
+    cell_type, cell_base = parser.set_cell(options)
+    genome = parser.set_genome(options)
+    
+    generator = GenomeResourcesSqlGenerator(cell_type=cell_type, genome=genome)
+    
+    print 'Creating genome resources database schema and tables...'
+    q = generator.all_sql()
+    if not options.schema_only: q += generator.fill_tables_mm9()
+    
+    execute_query(q)
+    
--- trunk/glasslab/glassatlas/pipeline/base_parser.py
+++ trunk/glasslab/glassatlas/pipeline/base_parser.py
@@ -10,7 +10,7 @@ from glasslab.utils.scripting import GlassOptionParser
 
 class GlassAtlasParser(GlassOptionParser):
     def set_cell(self, options):
-        cell_type = options.cell_type.lower() or current_settings.CELL_TYPE.lower()
+        cell_type = (options.cell_type and options.cell_type.lower()) or current_settings.CELL_TYPE.lower()
         cell_base = CellTypeBase().get_cell_type_base(cell_type)()
         current_settings.CELL_TYPE = cell_base.cell_type
         
