40332f3c6af10d49433bda3e05837ab452602828
Karmel Allison <karmel@arcaio.com>
2012-12-14 17:18:27 -0800

    - Bug fixing for new genome

--- glasslab/glassatlas/pipeline/set_up_refseq_database.py
+++ glasslab/glassatlas/pipeline/set_up_refseq_database.py
@@ -40,15 +40,16 @@ if __name__ == '__main__':
     execute_query(q)
     
     try:
+        """
         print 'Adding data...'
         path = os.path.join(get_glasslab_path(), 'glassatlas/pipeline/scripts')
         print subprocess.check_output(path + '/set_up_database.sh -g dm3 -c refseq', shell=True)
         print subprocess.check_output(path + '/transcripts_from_tags.sh -g dm3 -c refseq '
                                       + ' --schema_name=genome_reference_dm3 '
                                       + ' --tag_table=sequence_transcription_region '
-                                      #+ ' --stitch_processes=2 --set_density --draw_edges '
+                                      #+ ' --stitch --stitch_processes=2 --set_density --draw_edges '
                                       + ' --no_extended_gaps', shell=True)
-        
+        """
     except Exception, e: 
         print e
     finally:
@@ -57,4 +58,4 @@ if __name__ == '__main__':
         for chr_id in chr_ids:
             q += """DROP TABLE "{0}_{chr_id}";
                         """.format(SequenceTranscriptionRegion._meta.db_table, chr_id=chr_id)
-        execute_query(q)
+        #execute_query(q)
