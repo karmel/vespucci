aa8a0d660a45bf060f2612ee9832236a368b19b4
Karmel Allison <karmel@arcaio.com>
2013-04-17 17:26:07 -0700

    - Changing name

--- vespucci/sequencing/pipeline/scripts/add_peaks.sh
+++ vespucci/sequencing/pipeline/scripts/add_peaks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export PYTHONPATH=$CURRENT_PATH:$PYTHONPATH
-export DJANGO_SETTINGS_MODULE=glasslab.config.current_settings
+export DJANGO_SETTINGS_MODULE=vespucci.config.current_settings
 
-python $CURRENT_PATH/glasslab/sequencing/pipeline/add_peaks.py $@
+python $CURRENT_PATH/vespucci/sequencing/pipeline/add_peaks.py $@
