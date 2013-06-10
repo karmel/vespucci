a788d1d67d2905f12d70dbf19820018ca0829cc7
Karmel Allison <karmel@arcaio.com>
2013-06-10 09:30:35 -0700

    - Adding R script for tuning HMM

--- vespucci/analysis/hah_et_al/scripts/run_hmm.R
+++ vespucci/analysis/hah_et_al/scripts/run_hmm.R
@@ -1,7 +1,20 @@
 library(GROseq)
 
-# Get refseq data
-ref_path = '../../../genomereference/pipeline/data/mm9/mm9.bed'
-ref = read.table(ref_path, header=FALSE, sep="\t")
+# Get parameters
+args = commandArgs(trailing=TRUE)
+lt_prob = args[1]
+uts = args[2]
 
-#
+# Get data
+data_path = '../data/notx_chrX.txt'
+data = read.table(data_path, header=TRUE, sep='\t')
+
+# Run HMM with passed params
+hmm_tune = DetectTranscriptsEM(data, LtProbB=lt_prob, UTS=uts, thresh=1)
+
+# Write out to data file.
+write_path = paste('../data/hmm_transcripts_',lt_prob,'_',uts,'.txt',sep='')
+write.table(hmm_tune$Transcripts, file=write_path, sep='\t', 
+			row.names=FALSE,col.names=TRUE)
+			
+print(paste('Data saved to', write_path, sep=' '))
--- vespucci/analysis/hah_et_al/tune_hmm.py
+++ vespucci/analysis/hah_et_al/tune_hmm.py
@@ -14,4 +14,13 @@ cases where the shape setting was set to 5, 10, 15, or 20, and the –log of the
 transition probability between transcribed and nontranscribed was set to 
 100, 150, 200, 250, 300, or 500. Final values selected by the model include 
 a shape setting of 5 and a –log transition probability of 200."
+
+R library is called GROseq, and is run from an R script here.
 '''
+
+class HMMTuner(object):
+    r_path = 'scripts/run_hmm.R'
+    lts_probs = [50, 100, 150, 200, 250, 300, 500]
+    uts = [1, 5, 10, 15, 20]
+    
+    
