6d46e01d4f0f5991f82cb2aa782184ec5d28bdf5
Karmel Allison <karmel@arcaio.com>
2013-10-23 12:07:57 -0700

    Handling multiprocessing exceptions

--- vespucci/utils/database.py
+++ vespucci/utils/database.py
@@ -82,6 +82,7 @@ def execute_query_in_transaction(query,
     cursor = connection.cursor()
     cursor.execute(query)
     transaction.commit_unless_managed()
+    connection.close()
     if return_cursor: return cursor
 
 def fetch_rows(query, return_cursor=False, using='default'):
